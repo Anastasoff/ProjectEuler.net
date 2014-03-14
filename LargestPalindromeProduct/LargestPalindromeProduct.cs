49f3c880aeb57ea3039c22f408cf12f204410a73
Martin Anastasov <m4rt00@gmail.com>
2014-03-14 14:21:27 +0200

    create repo

---
+++ LargestPalindromeProduct/LargestPalindromeProduct.cs
@@ -0,0 +1,56 @@
+﻿// 4. A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+//  Find the largest palindrome made from the product of two 3-digit numbers.
+using System;
+
+class LargestPalindromeProduct
+{
+    static void Main()
+    {
+        int largest = 0;
+        int product = 0;
+        int firstDigit = 999;
+        int secondDigit = 999;
+        int currentDigit = secondDigit;
+
+        while (firstDigit > 0)
+        {
+            while (currentDigit > 0)
+            {
+                product = firstDigit * currentDigit;
+
+                if (IsPalindrome(product) && largest < product)
+                {
+                    largest = product;
+                }
+
+                currentDigit--;
+            }
+
+            firstDigit--;
+            currentDigit = secondDigit;
+        }
+
+        Console.WriteLine(largest); // 906609
+    }
+
+    static bool IsPalindrome(int number)
+    {
+        int palindrome = number; // copied number into variable
+        int reverse = 0;
+
+        while (palindrome != 0)
+        {
+            int remainder = palindrome % 10;
+            reverse = reverse * 10 + remainder;
+            palindrome = palindrome / 10;
+        }
+
+        // if original and reverse of number is equal means number is palindrome
+        if (number == reverse)
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ LargestPrimeFactor/LargestPrimeFactor.cs
@@ -0,0 +1,34 @@
+﻿// 3. The prime factors of 13195 are 5, 7, 13 and 29.
+//  What is the largest prime factor of the number 600851475143 ?
+using System;
+
+class LargestPrimeFactor
+{
+    static void Main()
+    {
+        long number = 600851475143;
+        long prime = 2;
+        long largest = 0;
+
+        while (number > 1)
+        {
+            if (number % prime == 0)
+            {
+                if (largest < prime)
+                {
+                    largest = prime;
+                }
+
+                number /= prime;
+                prime = 2;
+            }
+            else
+            {
+                prime++;
+            }
+        }
+
+        Console.WriteLine(largest); // 6857
+    }
+}
+
