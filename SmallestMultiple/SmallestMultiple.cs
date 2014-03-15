b993cc9db2f9270b6f141cc22e103006598b0875
Martin Anastasov <m4rt00@gmail.com>
2014-03-16 00:16:50 +0200

    add Smallest number

---
+++ SmallestMultiple/SmallestMultiple.cs
@@ -0,0 +1,33 @@
+﻿// 5. 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+//  What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+using System;
+class SmallestMultiple
+{
+    static void Main(string[] args)
+    {
+        checked
+        {
+            int counter = 1;
+            while (true)
+            {
+                for (int i = 1; i <= 20; i++)
+                {
+                    if (counter % i != 0)
+                    {
+                        break;
+                    }
+
+                    if (i == 20)
+                    {
+                        Console.WriteLine(counter); // 232792560
+                        return;
+                    }
+                }
+
+                counter++;
+            }
+        }
+    }
+}
+
