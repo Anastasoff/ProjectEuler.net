013f7311654aabc55aecf1711621d358eca2c3cc
Martin Anastasov <m4rt00@gmail.com>
2014-05-14 11:48:04 +0300

    update

---
+++ 10001stPrime/solution.js
@@ -0,0 +1,33 @@
+function isPrime(number) {
+    var isNumberPrime = true,
+        maxDevider = Math.sqrt(number);
+
+    for (var i = 2; i <= maxDevider; i++) {
+        if (number % i === 0) {
+            isNumberPrime = false;
+            break;
+        }
+    }
+
+    return isNumberPrime;
+}
+
+function findNhPrime(start, end) {
+    var primesList = [];
+
+    // TODO: while loop, counter, if counter == searched prime -> return counter
+    for (var i = start; i < end; i++) {
+        var isNumberPrime = isPrime(i);
+        if (isNumberPrime) {
+            primesList.push(i);
+        }
+    }
+
+    return primesList;
+}
+
+var result = findNhPrime(1, 105000);
+
+for (var i = result.length - 30; i < result.length; i++) {
+    console.log(i + ' > ' + result[i]);
+}
